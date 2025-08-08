a3af59ef34c77cc92f12f7e1877120770f1780e8
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2025-08-08 22:14:58 +0100

    fix(ci) remove AWS CLI profile dependency from providers.tf for Terraform Cloud compatibility
    - Removed 'profile = default' from AWS provider to enable authentication via environment variables in Terraform Cloud
    - Minor formatting adjustments to kubernetes provider block for consistency

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -2,12 +2,11 @@
 
 provider "aws" {
   region  = "eu-west-1"
-  profile = "default"
 }
 
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
+  host = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority)
-  token                  = data.aws_eks_cluster_auth.main.token
+  token = data.aws_eks_cluster_auth.main.token
 }
