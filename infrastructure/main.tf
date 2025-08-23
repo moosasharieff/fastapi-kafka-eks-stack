704b43ff7fd5bbba70a4d905db7fa1951b43cbb7
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2025-08-23 21:40:16 +0100

    feat(infra): wire eks_ssh_key module into EKS & SG; add tfvars
    - Passed ec2_ssh_key from eks_ssh_key module to EKS module
    
    - Provided eks_worker_sg_id to security_groups module
    
    - Declared eks_ssh_key module with key_name and private_key_path
    
    - Updated terraform.tfvars with key_name and private_key_path

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,6 +13,7 @@ module "eks" {
   cluster_name       = var.cluster_name
   private_subnet_ids = module.vpc.private_subnet_ids
   eks_instance_type  = var.eks_instance_type
+  ec2_ssh_key        = module.eks_ssh_key
 
 }
 
@@ -20,4 +21,12 @@ module "security_groups" {
   source = "./modules/security_groups"
 
   vpc_id = module.vpc.vpc_id
+  eks_worker_sg_id = module.eks.worker_sg_id
+
+}
+
+module "eks_ssh_key" {
+  source = "./modules/ssh_key"
+  key_name = "${var.project}-${var.key_name}"
+  private_key_path = var.private_key_path
 }
--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -3,3 +3,7 @@
 project           = "fastapi-kafka-eks-stack"
 cluster_name      = "moosa"
 eks_instance_type = ["t2.micro"]
+
+# Key Pair Variables
+key_name = "key-pair"
+private_key_path = "~/.ssh/id_rsa_eks"
